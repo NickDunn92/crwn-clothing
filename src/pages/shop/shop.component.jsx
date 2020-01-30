fe0beff23af32284dbea6811c088ddf6d55fd7dd
NickDunn92 <nickdunn92@icloud.com>
2020-01-30 17:05:10 +0000

    update our menu-items to navigate with Router HOC, updating app with a shop page, collection preivew component and collection item component

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import ShopData from './shop.data';
+
+import CollectionPreview from '../../components/collection-preview/collection-preview'
+
+class ShopPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ShopData
+    }
+  }
+
+  render() {
+    const {ShopData} = this.state;
+    return (<div className='shop-page'>
+      {
+        ShopData.map(({ id, ...ShopData }) => (
+          <CollectionPreview key={id} {...ShopData} />
+        ))
+      }
+    </div>);
+  }
+}
+
+export default ShopPage;
